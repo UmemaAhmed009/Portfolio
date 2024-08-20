e02f97f8ee43de11bf04b61e2de6d20c8209b278
syedaumema ahmed <umemaahmed009@gmail.com>
2024-08-20 12:44:31 +0500

    about me section responsiveness improved

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import image from "../assets/bg.png"
 
 export function AboutMe(){
-    // px-4 py-16 mb-16 mx-auto max-w-screen-xl bg-[url('./assets/bg.png')] bg-cover bg-center sm:bg-right-top md:bg-center lg:bg-cover min-h-screen"
+    // px-4 py-16 mb-16 mx-auto max-w-screen-xl bg-[url('./assets/bg.png')] bg-cover bg-center sm:bg-right-top md:bg-center lg:bg-cover min-h-screen "
     return(
-        <section id="about-me" className="px-4 py-12 mb-4 mx-2 lg:mx-auto max-w-screen-xl bg-[url('./assets/bg.png')] bg-contain bg-no-repeat bg-center md:bg-cover sm:bg-right-top md:bg-center lg:bg-cover min-h-screen sm:px-8 md:px-16 lg:px-24 xl:px-32">
+        <section id="about-me" className="px-4 py-12 mb-2 mx-2 lg:mx-auto max-w-screen-xl bg-[url('./assets/bg.png')] bg-contain bg-no-repeat bg-center sm:bg-cover sm:min-h-screen ">
         </section>
         // <section
     //   id="about-me"
